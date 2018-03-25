221072d36a1da2444c0d84cd2b124c839cef048b
Laura Costinas <laura_costinas@yahoo.com>
2018-03-25 18:29:10 +0300

    Modified test class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,7 @@
 import controller.StoreController;
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import model.Product;
 import repository.StoreRepository;
 
@@ -21,5 +23,31 @@ public class AppTest extends TestCase {
 
 
         assertEquals(2,allProducts.size());
+    }
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
